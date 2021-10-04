db632067d43d68e3f47a81ea88f10bade4094f21
Tio Marco (TioDarius1) <91704772+TioMarc0@users.noreply.github.com>
2021-10-04 11:05:58 -0300

    Create parImpar.java

---
+++ src/javaProjectMarco/parImpar.java
@@ -0,0 +1,10 @@
+package javaProjectMarco;
+
+public class parImpar {
+//Faça um Programa que peça um número inteiro e se este número for par, transforme-o em impar e vice-versa.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
